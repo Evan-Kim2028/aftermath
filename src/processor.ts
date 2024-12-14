40a627d5bf9a0f5d9cc2fae0fb8dcd282668d3b8
Evan-Kim2028 <ekcopersonal@gmail.com>
2024-12-13 20:26:52 -0500

    feat: add checkpoint, adjust formatting

--- src/processor.ts
+++ src/processor.ts
@@ -1,19 +1,22 @@
 import { SuiObjectTypeProcessor } from "@sentio/sdk/sui"
 import { pool } from "./types/sui/0xefe170ec0be4d762196bedecd7a065816576198a6527c99282a2551aaa7da38c.js"
 
-
 SuiObjectTypeProcessor.bind({
-  objectType: pool.Pool.type()
+  objectType: pool.Pool.type(),
+  startCheckpoint: 6684001n
 })
 .onTimeInterval(async (self, _, ctx) => {
   if (!self) { return }
   console.log(`ctx ${ctx.objectId} ctx.timestamp ${ctx.timestamp}`)
 
-  const balanceOf = await pool.view.balanceOf(ctx.client, [ctx.objectId], [pool.Pool.type(), pool.Pool.type()])
+  const balanceOf = await pool.view.balanceOf(
+    ctx.client, 
+    [ctx.objectId], 
+    [pool.Pool.type(), pool.Pool.type()]
+  )
 
   // Emit event with all values
   ctx.eventLogger.emit("pool_info", {
     name: balanceOf,
-
   })
 })
